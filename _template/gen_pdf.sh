4cf61cbdbcd8ed5759c06ec2f66f8fb9b06c93ac
Geoffrey Howland <geoff@gmail.com>
2026-02-14 14:52:23 +0700

    Update All

--- _template/gen_pdf.sh
+++ _template/gen_pdf.sh
@@ -46,8 +46,11 @@ sed -i 's/\$\$/$/g' manuscript_fixed.md
 # 5. Fix list formatting: Force a newline before any hyphen preceded by text
 sed -i 's/\([[:alnum:]\)]\)- /\1\n- /g' manuscript_fixed.md
 
-# Double Space - Toggle Commenting as needed
-sed -i '/^$/d;G' manuscript_fixed.md
+# # Double Space - Toggle Commenting as needed
+# sed -i '/^$/d;G' manuscript_fixed.md
+
+# Double Space - Only if line does not start with |
+sed -i '/^|/!{/^$/d;G}' manuscript_fixed.md
 
 # 6. Run Pandoc
 # Added amssymb and amsmath to ensure symbols like \checkmark and \times are recognized
