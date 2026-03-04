b3c7f3f3da2ad57a5c41acafb6ec04200151d3a3
Geoffrey Howland <geoff@gmail.com>
2026-03-04 07:03:54 +0700

    Update All

--- _template/cks_tools/control_system.py
+++ _template/cks_tools/control_system.py
@@ -9,15 +9,28 @@ import sys
 
 COMMANDS = ['list', 'show']
 
+
+def List(args):
+  print("List everything")
+
+def Show(args):
+  print("Show something")
+
 def Main(args):
   if args.verbose:
-      print(f"Verbosity is enabled.")
-      print(f"Positional argument value: {args.input}")
-
-  # Example of using the numeric argument
-  result = args.count * 2
-  if args.verbose:
-      print(f"Calculation result (count * 2): {result}")
+    print(f"Verbosity is enabled.")
+  
+  if args.command not in COMMANDS:
+    print(f'Unknown command: {args.command}')
+    exit(1)
+  
+  # List
+  if args.command == 'list':
+    List(args)
+  
+  # Show
+  if args.command == 'show':
+    Show(args)
 
 
 
@@ -25,7 +38,7 @@ def Main(args):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="Control System for safely handling large counts of Zenodo papers")
 
-  parser.add_argument("command", help="Action to perform: ^s" )
+  parser.add_argument("command", help="Action to perform: %s" % ', '.join(COMMANDS) )
   parser.add_argument("-n", "--name", type=str, default="User", help="The name to greet (default: User)")
   parser.add_argument("-c", "--count", type=int, default=1, help="A numeric repeat count (default: 1)")
   parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity")
