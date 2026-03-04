1b15b5fecd9d1c3dcc3b7206d8a79f4257d373e4
Geoffrey Howland <geoff@gmail.com>
2026-03-04 07:21:33 +0700

    Update All

--- _template/cks_tools/control_system.py
+++ _template/cks_tools/control_system.py
@@ -7,25 +7,34 @@ Control System for safely handling large counts of Zenodo papers
 import argparse
 import sys
 import os
+import json
 
 COMMANDS = ['list', 'show']
 
 WORKING_DIR = '/mnt/c/Users/Geoff/cks/cks'
 
 WORKING_PATH_SET = '_template/cks_tools/paper_work_list.txt'
+AUDIT_SET = '_template/cks_tools/zenodo_master_manifest.json'
+
 
 def List(args):
   print("List everything")
-  print(args.work_list)
+  # print(args.work_list)
+  print(args.zenodo)
+
 
 def Show(args):
   print("Show something")
+
 
 def Main(args):
   os.chdir(WORKING_DIR)
 
   # Save our work list data
   args.work_list = open(WORKING_PATH_SET).read().strip().split('\n')
+
+  with open(AUDIT_SET, "r", encoding="utf-8") as fp:
+    args.zenodo = json.load(fp)
 
   if args.verbose:
     print(f"Verbosity is enabled.")
