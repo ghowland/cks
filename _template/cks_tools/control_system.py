723fa5ad439030e9464d1ae17f9923d6161bb916
Geoffrey Howland <geoff@gmail.com>
2026-03-05 14:33:16 +0700

    Update All

--- _template/cks_tools/control_system.py
+++ _template/cks_tools/control_system.py
@@ -82,6 +82,8 @@ def Build(args):
       cmd = f'{GEN_PDF} {directory}' 
       print(cmd)
 
+      # if item['paper_id'] != 'CKS-MATH-123-2026': continue # Skip test
+ 
       (status, output, error) = execute_command(cmd)
       print(f'  Result: {status}  Output: {output[:40]}')
 
