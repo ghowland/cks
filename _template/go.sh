f8e9fcec2b76bd39ff75daedcc3cfad30442fc78
Geoffrey Howland <geoff@gmail.com>
2026-03-12 18:27:29 +0700

    Update All

--- _template/go.sh
+++ _template/go.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
- python3 _template/cks_tools/audit_paper.py --papers-dir ./ ; ./_template/cks_tools/control_system.py cleanup ; ./_template/cks_tools/control_system.py gen
+_template/_old/create_bibs.py
 
+_template/_old/scan.py
 
- ./_template/cks_tools/control_system.py scan
+python3 _template/cks_tools/audit_paper.py --papers-dir ./
 
- ./_template/cks_tools/control_system.py build
+./_template/cks_tools/control_system.py cleanup
+
+./_template/cks_tools/control_system.py gen
+
+./_template/cks_tools/control_system.py scan
+
+./_template/cks_tools/control_system.py build
 
--- report.md
+++ report.md
@@ -1,6 +1,6 @@
 # CKS Papers Audit Report
 
-**Generated:** 2026-03-12 11:12:21 UTC
+**Generated:** 2026-03-12 11:26:07 UTC
 
 ## Summary
 
